bbc8bf620f14ab187de1544a7567bc366926a026
PietroDanieli <1pie7d@gmail.com>
2022-09-04 06:15:26 +0200

    complete CarouselInfinityThumbs as third carousel

--- js/carousel.js
+++ js/carousel.js
@@ -60,47 +60,128 @@ const carouselFinishContainer = document.getElementById(
   "carouselFinishContainer"
 );
 
-const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+const imagesInfinity = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+const imagesInfinityThumbs = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
+  "06.jpg",
+  "07.jpg",
+  "08.jpg",
+  "09.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+  "18.jpg",
+];
 let imagesIndex = 0;
+let thumbsIndex = 0;
 
 const carouselInfinityContainer = document.getElementById(
   "carouselInfinityContainer"
 );
+const carouselInfinityThumbsContainer = document.getElementById(
+  "carouselInfinityThumbsContainer"
+);
+const imageContainer = document.getElementById("imageContainer");
+
+// EVENT CLICK PREVIUS
 document.getElementById("previous").addEventListener("click", function () {
+  // Call Active Carousel Container
   const activeElement = document.querySelector(
-    ".carouselContainer.active > .item.active"
+    ".carouselContainer.active .item.active"
   );
+
+  // Istruction for FinishCarousel and general carousel
   const previousElement = activeElement.previousElementSibling;
   if (previousElement) {
     activeElement.classList.remove("active");
     previousElement.classList.add("active");
   }
+
+  // Istruction for InfinityCarousel Carousel
+
+  //  reset of node
   carouselInfinityContainer.innerHTML = "";
   if (imagesIndex > 0) {
     imagesIndex--;
   } else {
-    imagesIndex = images.length - 1;
-    console.log("guarda me");
+    imagesIndex = imagesInfinity.length - 1;
   }
 
-  for (let index = 0; index < images.length; index++) {
+  for (let index = 0; index < imagesInfinity.length; index++) {
     let activeClass = "";
     if (index === imagesIndex) {
       activeClass = "active";
     }
-    carouselInfinityContainer.innerHTML += `<img class="item ${activeClass}" src="img/0${images[index]}" alt"" />`;
+    carouselInfinityContainer.innerHTML += `<img class="item ${activeClass}" src="img/0${imagesInfinity[index]}" alt"" />`;
+  }
+  // Istruction for InfinityThumbsCarousel Carousel
+  imageContainer.innerHTML = "";
+  if (thumbsIndex > 0) {
+    thumbsIndex--;
+  } else {
+    thumbsIndex = imagesInfinityThumbs.length - 1;
+  }
+
+  for (let index = 0; index < imagesInfinityThumbs.length; index++) {
+    let activeClass = "";
+    if (index === thumbsIndex) {
+      activeClass = "active";
+    }
+    imageContainer.innerHTML += `<img class="item ${activeClass}" src="img/00${imagesInfinityThumbs[index]}" alt"" />`;
   }
 });
 
 // collego pulsante next
 
+// Istruction for FinishCarousel and general carousel
 document.getElementById("next").addEventListener("click", function () {
   const activeElement = document.querySelector(
-    ".carouselContainer.active > .item.active"
+    ".carouselContainer.active .item.active"
   );
   const nextElement = activeElement.nextElementSibling;
   if (nextElement) {
     activeElement.classList.remove("active");
     nextElement.classList.add("active");
+  }
+  // Istruction for InfinityCarousel Carousel
+
+  //  reset of node
+  carouselInfinityContainer.innerHTML = "";
+  if (imagesIndex > 0) {
+    imagesIndex--;
+  } else {
+    imagesIndex = imagesInfinity.length - 1;
+  }
+
+  for (let index = 0; index < imagesInfinity.length; index++) {
+    let activeClass = "";
+    if (index === imagesIndex) {
+      activeClass = "active";
+    }
+    carouselInfinityContainer.innerHTML += `<img class="item ${activeClass}" src="img/0${imagesInfinity[index]}" alt"" />`;
+  }
+  // Istruction for InfinityThumbsCarousel Carousel
+  imageContainer.innerHTML = "";
+  if (thumbsIndex > 0) {
+    thumbsIndex--;
+  } else {
+    thumbsIndex = imagesInfinityThumbs.length - 1;
+  }
+
+  for (let index = 0; index < imagesInfinityThumbs.length; index++) {
+    let activeClass = "";
+    if (index === thumbsIndex) {
+      activeClass = "active";
+    }
+    imageContainer.innerHTML += `<img class="item ${activeClass}" src="img/00${imagesInfinityThumbs[index]}" alt"" />`;
   }
 });
